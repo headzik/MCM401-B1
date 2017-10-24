305b1fca42a6b93bb07b53ad1be665a33959a648
Bernhard Hiesl <bernhard.hiesl@myalfred.io>
2017-10-24 16:53:43 +0200

    created new classes

---
+++ src/at/fhooe/mcm/components/GISComponent.java
@@ -0,0 +1,17 @@
+package at.fhooe.mcm.components;
+
+import at.fhooe.mcm.interfaces.IComponent;
+
+import java.awt.*;
+
+public class GISComponent implements IComponent {
+    @Override
+    public Panel getView() {
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+}
