3d0e6ff90eac8ceae90501888ede9a4df5029684
Bernhard Hiesl <bernhard.hiesl@myalfred.io>
2017-10-24 16:29:19 +0200

    edited ignorefile for intellij

--- src/at/fhooe/mcm/Mediator.java
+++ src/at/fhooe/mcm/Mediator.java
@@ -1,4 +1,32 @@
 package at.fhooe.mcm;
 
-public class Mediator {
+import at.fhooe.mcm.components.POIComponent;
+import at.fhooe.mcm.interfaces.IComponent;
+import at.fhooe.mcm.interfaces.IMediator;
+import at.fhooe.mcm.views.MediatorView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Mediator implements IMediator {
+
+    private MediatorView mMediatorView;
+    private List<IComponent> mComponents;
+
+    public Mediator() {
+        mMediatorView = new MediatorView();
+        mComponents = new ArrayList<>();
+
+        POIComponent p = new POIComponent();
+        mComponents.add(p);
+
+        mMediatorView.addTabs(mComponents);
+    }
+
+    public static void main(String[] args) {
+
+        Mediator m = new Mediator();
+
+
+    }
 }
