7253723f2ad7301c84bcb0d238eb014f5d3d8f5c
ifumi <ds230694@gmail.com>
2017-10-31 11:44:21 +0100

    wos a imma

--- src/at/fhooe/mcm/gps/GPSModel.java
+++ src/at/fhooe/mcm/gps/GPSModel.java
@@ -17,6 +17,9 @@ public class GPSModel extends Observable implements PositionUpdateListener {
 		} catch (FileNotFoundException _e1) {
 			_e1.printStackTrace();
 		}		
+		
+		System.out.println(GPSModel.lat2y(48.369484d));
+		System.out.println(GPSModel.lon2x(14.512067d));
 	}
 	
 	public NMEAParser getParser() {
@@ -31,8 +34,7 @@ public class GPSModel extends Observable implements PositionUpdateListener {
 	
 	public void stopParsing() {
 		if (t != null)
-			t.interrupt();
-	}
+			t.interrupt();	}
 
 	@Override
 	public void updateSats(NMEAInfo _mInfo) {
@@ -41,5 +43,13 @@ public class GPSModel extends Observable implements PositionUpdateListener {
 		// TODO --> CONVERT LONGITUDE AND LATITUDE TO SCREEN COORDINATES AND SET NEW POINT OF POSITION POI OBJECT.
 		
 		notifyObservers(position); // Notify that position has changed
+	}
+	
+	public static final double lat2y(double aLat) {
+	    return ((1 - Math.log(Math.tan(aLat * Math.PI / 180) + 1 / Math.cos(aLat * Math.PI / 180)) / Math.PI) / 2 * Math.pow(2, 0)) * 256 * 1000;
+	}
+	
+	public static final double lon2x(double lon) {
+	    return (lon + 180f) / 360f * 256f * 1000;
 	}
 }
